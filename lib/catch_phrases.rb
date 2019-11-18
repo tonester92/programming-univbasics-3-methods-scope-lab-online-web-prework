9b540af431a94515812cc13df6584fc30bc0e6c0
Antonio Blandin <antonio.blandin25@gmail.com>
2019-11-18 06:16:19 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,14 @@ end
 def toadstool
   puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end 
+
+def any_phrase(phrase = "Do A BArrel Roll!")
+  puts phrase 
+end 
+
+  
+  
